a156eaa1756c320f8e4f8f8ddfba799bf142d2fd
Ephy Kimotho <ephyk48@gmail.com>
2025-02-24 14:45:28 +0300

    styles: added background image and shdow custom config for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+        quciksand: ["Quicksand", "sans-serif"],
+        mulish: ["Mulish", "sans-serif"],
+      },
+      colors: {
+        emerald: "#06945d",
+        grey: "#8E8E90",
+        moldGreen: "#1F4E3C",
+      },
+      backgroundImage: {
+        farmImage: "url('/images/farmImage.png')",
+        aboutImage: "url('/images/about.jpg')",
+        smallPlant: "url('/images/smallPlant.jpg')",
+        dairy: "(url('/images/dairy.png'))",
+      },
+      boxShadow: {
+        overlay: "inset 0 0 0 1000px rgba(0,0,0,0.75)",
+      },
+    },
   },
   plugins: [],
 };
