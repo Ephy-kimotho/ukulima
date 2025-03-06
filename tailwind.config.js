7fb9dc74fb11d55e3a792aca5a64b830691522d4
Mash <marshmisherk@gmail.com>
2025-03-06 13:16:40 +0300

    feat:Implemented the Contact Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const {fontFamily} = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+
+      fontFamily:{
+        nunito:["Nunito", "sans-serif"]
+      },
+
+      backgroundImage: {
+        contact: "url('/images/contact3.png')"
+      },
+
+      boxShadow: {
+        overlay: "inset 0 0 0 1000px rgba(0,0,0,0.70)"
+      }
+    },
   },
   plugins: [],
 };
