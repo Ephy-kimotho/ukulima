7ab5036f7c81d49dbc069562fd58bf003c12223e
Ephy Kimotho <ephyk48@gmail.com>
2025-03-14 16:53:14 +0300

    feat: add product components and built products page

---
+++ src/components/products/ProductDetail.jsx
@@ -0,0 +1,78 @@
+import { useParams, Link } from "react-router-dom";
+import { ArrowLeft } from "lucide-react";
+import { BASE_URL } from "../../constants";
+import Button from "../common/Button";
+import useAxios from "../../hooks/useAxios";
+
+function ProductDetail() {
+  const { id } = useParams();
+
+  const { data: selectedItem, isLoading: loading } = useAxios(
+    `${BASE_URL}/products/${id}`
+  );
+
+  // TODO: write add to cart function.
+  const addItemToCart = () => {
+    console.log("Item added to cart");
+  };
+
+  return (
+    <section className="bg-[#fdfbf0] min-h-screen py-10 px-5 text-night font-nunito">
+      {loading ? (
+        <div className="min-h-screen grid place-items-center">
+          <p className="text-slate-800 text-3xl font-bold">Loading product..</p>
+        </div>
+      ) : (
+        <>
+          <Link
+            to=".."
+            relative="path"
+            className="ml-5 hover:border-b hover:border-[#2d3134] space-x-1 inline-flex items-center "
+          >
+            <ArrowLeft size={26} color="#2d3134" />
+            <span className="text-lg text-[#2d3134] font-semibold">Back</span>
+          </Link>
+
+          <div className="flex flex-col gap-6 mt-6 md:flex-row items-center md:justify-around ">
+            {selectedItem && (
+              <img
+                src={selectedItem.image}
+                alt={selectedItem.name}
+                className="w-1/3 self-stretch rounded"
+              />
+            )}
+            <ul className="bg-[#D9D9D9] self-stretch text-night p-8 gap-4 lg:gap-12 rounded-md text-xl flex flex-1 flex-col md:justify-center list-none">
+              <li>
+                <span className="font-bold">Name: </span>
+                <span>{selectedItem?.name}</span>
+              </li>
+              <li>
+                <span className="font-bold">Availability: </span>
+                <span>{selectedItem?.category}</span>
+              </li>
+              <li>
+                <span className="font-bold">Price: </span>
+                <span> Ksh. {selectedItem?.price}</span>
+              </li>
+              <li>
+                <span className="font-bold">Description: </span>
+                <span>{selectedItem?.description}</span>
+              </li>
+            </ul>
+          </div>
+
+          <div className="text-center mt-20">
+            <Button
+              action={addItemToCart}
+              moreStyles="font-bold text-white px-20 py-4 hover:bg-emerald rounded-xl bg-mint text-xl active:scale-95 cursor-pointer tracking-wider"
+            >
+              Add to cart
+            </Button>
+          </div>
+        </>
+      )}
+    </section>
+  );
+}
+
+export default ProductDetail;
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,8 +1,83 @@
+import ProductListing from "./ProductListing";
+import useAxios from "../../hooks/useAxios";
+import { BASE_URL } from "../../constants";
+
 function Products() {
+  const { data, isLoading, error } = useAxios(`${BASE_URL}/products`);
+  const { data: categories } = useAxios(`${BASE_URL}/categories`);
+
+  if (error) {
+    console.error(error);
+  }
+
   return (
-    <div>
-      <p>This is the Products page</p>
-    </div>
+    <section className="bg-white min-h-screen pt-6 pb-20">
+      <div className="text-center">
+        <h2 className="font-montserrat text-mint font-bold  text-3xl md:text-5xl">
+          Explore our Products
+        </h2>
+        <p className="font-poppins text-night font-medium text-base md:text-xl">
+          Click a product to view details
+        </p>
+      </div>
+
+      {isLoading ? (
+        <div className="h-screen grid place-items-center">
+          <p className="text-3xl text-night font-nunito font-bold">
+            Loading products...
+          </p>
+        </div>
+      ) : (
+        <div>
+          <div className="flex flex-col items-center gap-4 md:gap-0 md:flex-row md:justify-between max-w-[90%] mx-auto my-10">
+            <div className="bg-[#F0F0F0] w-full md:w-max border border-black rounded-lg p-2 flex items-center gap-2">
+              <label
+                htmlFor="filter-select"
+                className="text-base text-night/50"
+              >
+                Filter by:
+              </label>
+              <select
+                name="filter-select"
+                id="filter-select"
+                className="text-night cursor-pointer flex-1 md:flex-none  outline-none"
+              >
+                <option value="" selected>
+                  -- choose category --
+                </option>
+                {categories?.map((category, idx) => (
+                  <option key={idx} value={category.name}>
+                    {category.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <div className="bg-lightGrey w-full md:w-max  border border-black rounded-lg p-2 flex items-center gap-2">
+              <label
+                htmlFor="filter-select"
+                className="text-base text-night/50"
+              >
+                Sort by price:
+              </label>
+              <select
+                name="filter-select"
+                id="filter-select"
+                className="text-night flex-1 cursor-pointer outline-none"
+              >
+                <option value="" disabled selected>
+                  -- &nbsp; choose &nbsp; --
+                </option>
+                <option value="ascending">Ascending</option>
+                <option value="descending">Descending</option>
+              </select>
+            </div>
+          </div>
+
+          <ProductListing results={data || []} />
+        </div>
+      )}
+    </section>
   );
 }
 
