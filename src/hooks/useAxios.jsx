890dac2376cbc181dc7e4ace738db86c7f3eaace
Ephy Kimotho <ephyk48@gmail.com>
2025-03-14 16:50:13 +0300

    chore: added new constants and hooks directories

---
+++ src/hooks/useAxios.jsx
@@ -0,0 +1,33 @@
+/* CUSTOM HOOK TO FETCH DATA FROM API */
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function useAxios(url) {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    /* TODO: simulate a delay in order to show skeletons */
+    setIsLoading(true);
+
+    setTimeout(() => {
+      axios
+        .get(url, { headers: { Accept: "application/json" } })
+        .then((res) => {
+          console.log(res.data);
+          setData(res.data);
+        })
+        .catch((err) => {
+          setError(err);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }, 500);
+  }, [url]);
+
+  return { data, isLoading, error };
+}
+
+export default useAxios;
