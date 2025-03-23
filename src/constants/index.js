229c39ce860c49bc28ef75455509ba58e0b3e11a
Ephy Kimotho <ephyk48@gmail.com>
2025-03-23 16:57:22 +0300

    chore: added action types for product management

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,9 @@
+/* --------- BASE URL  ---------*/
 export const BASE_URL = "http://localhost:3000";
+
+/* --------- PRODUCT ACTION TYPES   ---------*/
+export const REQUESTING = "REQUESTING";
+export const ON_SUCCESS = "ON_SUCCESS";
+export const ON_ERROR = "ON_ERROR";
+
+/* --------- CART ACTION TYPES   ---------*/
