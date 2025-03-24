6a4152a2d5d49e3a062fa4995b6131e5e9564763
Ephy Kimotho <ephyk48@gmail.com>
2025-03-24 19:03:20 +0300

    chore: added cart action types

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,3 +7,6 @@ export const ON_SUCCESS = "ON_SUCCESS";
 export const ON_ERROR = "ON_ERROR";
 
 /* --------- CART ACTION TYPES   ---------*/
+export const ADD_TO_CART = "ADD_TO_CART";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+export const CHANGE_QUANTITY = "CHANGE_QUANTITY";
