e0279461b38437bbe8b9aa5a24d6d424e43a287c
Ephy Kimotho <ephyk48@gmail.com>
2025-03-24 19:02:18 +0300

    fix: rename index.jsx to index.js and added saveToLocalStorage function

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,3 +7,7 @@ export function descendingSort(products) {
   const arr = products.slice();
   return arr.sort((a, b) => b.price - a.price);
 }
+
+export function saveToLocalStorage(name, item) {
+  localStorage.setItem(name, JSON.stringify(item));
+}
