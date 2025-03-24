10dd0cb3a6dd27575641e83cc81181858eb6afeb
Ephy Kimotho <ephyk48@gmail.com>
2025-03-24 19:05:26 +0300

    chore: included cart reducer in root reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { producerReducer } from "./products/productReducer";
+import { cartReducer } from "./cart/cartReducer";
 
 const rootReducer = combineReducers({
   products: producerReducer,
+  shoppingCart: cartReducer,
 });
 
 export default rootReducer;
