a6e019ce52a8b11890c3f5e49083fda54b5a2521
Ephy Kimotho <ephyk48@gmail.com>
2025-03-30 21:10:45 +0300

    feat: implemented mock logout

--- src/components/profile/ProfilePage.jsx
+++ src/components/profile/ProfilePage.jsx
@@ -1,33 +1,56 @@
+import { useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 import { PowerIcon } from "lucide-react";
 import { FaUserCircle } from "react-icons/fa";
+import toast from "react-hot-toast";
 
 function ProfilePage() {
-  //Js here
+  const { user, setToken } = useAuth();
+  const navigate = useNavigate();
+
+  const logout = () => {
+    setToken(false);
+    toast.success("Log out successful.");
+
+    setTimeout(() => {
+      navigate("/");
+    }, 0);
+  };
+
   return (
     <section className="bg-stone-200 min-h-screen font-nunito relative">
       <div className="">
         <article className="pt-16 mb-8 flex flex-col items-center">
           <FaUserCircle color="#000" size={120} />
           <p className="text-stone-900 text-4xl font-semibold">
-            Hello John Doe
+            Hello {user.firstName}
           </p>
         </article>
 
         <div className="w-11/12 max-w-md space-y-6  mx-auto text-2xl">
           <p className="text-black border-b-2 border-black">
             <span className="font-semibold">Full Name:</span>
-            <span className="pl-2"> John Doe</span>
+            <span className="pl-2">
+              {user.firstName} {user.lastName}
+            </span>
           </p>
 
           <p className="text-black flex border-b-2 border-black">
             <span className="font-semibold">Email:</span>
-            <span className="pl-2"> johndoe@gmail.com</span>
+            <span className="pl-2"> {user.email}</span>
+          </p>
+          <p className="text-black flex border-b-2 border-black">
+            <span className="font-semibold">Phone:</span>
+            <span className="pl-2"> {user.phone}</span>
           </p>
         </div>
 
         {/* LOG OUT BUTTON */}
         <div className="absolute bottom-10 left-0 right-0 flex justify-center">
-          <button className="flex items-center py-3 justify-center text-2xl font-bold rounded-xl gap-2 w-52 bg-moldGreen tracking-wider hover:scale-105 cursor-pointer active:scale-95 text-white">
+          <button
+            onClick={logout}
+            className="flex items-center py-3 justify-center text-2xl font-bold rounded-xl gap-2 w-52 bg-moldGreen tracking-wider hover:bg-emerald cursor-pointer active:scale-95 text-white"
+          >
             <PowerIcon /> Log out
           </button>
         </div>
