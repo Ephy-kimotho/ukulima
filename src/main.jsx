5516e9a64153f5498a85a06526e37aa68fbe10d7
Ephy Kimotho <ephyk48@gmail.com>
2025-03-30 21:12:25 +0300

    chore: updated favicon and provided AuthProvider to app

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
+import AuthProvider from "./Providers/AuthProvider.jsx";
 import store from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </Provider>
   </StrictMode>
 );
